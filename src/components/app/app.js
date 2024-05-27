a223c0a99cc88ab77d2963aa74ab3f95db13ed71
Liudmyla <arahna41@gmail.com>
2024-05-27 23:22:57 +0300

    lifting stage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,6 @@
+import {Component} from "react";
+import nextId from "react-id-generator";
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +9,97 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John', salary: 1000, increase: false, rise: true, id: 1},
+                {name: 'David', salary: 800, increase: true, rise: false, id: 2},
+                {name: 'Alex', salary: 1200, increase: false, rise: false, id: 3}
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'John', salary: 1000, increase: true, id: 1},
-        {name: 'David', salary: 800, increase: false, id: 2},
-        {name: 'Alex', salary: 1200, increase: false, id: 3}
-    ]
-  return (
-    <div className="app">
-        <AppInfo />
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            const newArr = data.filter(item => item.id !== id);
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const idEmployee = nextId();
+
+        const newEmployee = {
+            name: name,
+            salary: salary,
+            increase: false,
+            rise: false,
+            id: idEmployee
+        }
+
+        this.setState(({data}) => {
+            const newArr = structuredClone(data);
+            newArr.push(newEmployee);
+
+            return {
+                data: newArr
+            }
+        })
+    }
+
+    onToggleIncrease = (id) => {
+        // Option 1
+        // this.setState(({data}) => {
+        //     const index = data.findIndex(elem => elem.id === id);
+        //
+        //     const old = data[index];
+        //     const newItem = {...old, increase: !old.increase};
+        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+        //
+        //     return {
+        //         data: newArr
+        //     }
+        // })
+
+        // Option 2
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, increase: !item.increase}
+                }
+                return item;
+            })
+        }))
+    }
+
+    onToggleRise = (id) => {
+        console.log(`Rise is ${id}`)
+    }
+    render() {
+        const {data} = this.state;
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList
+                    data={data}
+                    onDelete={this.deleteItem}
+                    onToggleIncrease = {this.onToggleIncrease}
+                    onToggleRise = {this.onToggleRise}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -15,13 +15,23 @@ class EmployeesAddForm extends Component {
            [e.target.name]: e.target.value
         })
     }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
     render() {
         const {name, salary} = this.state;
         return (
             <div className="app-add-form">
                 <h3>Add new employee</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit} >
                     <input type="text"
                            className="form-control new-post-label"
                            placeholder="Name"
